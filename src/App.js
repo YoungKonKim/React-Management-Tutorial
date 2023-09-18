b2363d6d8805fc364f9e68437ce20f156cb7b10e
youngkon <ykkim708@gmail.com>
2023-09-18 21:50:13 +0900

    Custmer.js를 간단히 만들었다

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Customer from "./components/Customer";
+
+const customers = [
+  {
+    id: 1,
+    name: "홍길동",
+    birthday: "991231",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    name: "이순신",
+    birthday: "881231",
+    gender: "남자",
+    job: "프로그래머",
+  },
+  {
+    id: 3,
+    name: "홍문도",
+    birthday: "771231",
+    gender: "남자",
+    job: "디자인어",
+  },
+];
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} alt="logo" />
-      <h2>Let's develope Management System!</h2>
+    <div>
+      {customers.map((c) => {
+        return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+        );
+      })}
     </div>
   );
 }
